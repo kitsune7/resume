59efd9ff5ca4efe24226a5082114b0c0a4d77ae7
Christopher Bradshaw <chris.kofox@gmail.com>
2021-10-06 21:09:41 -0600

    Create resume for BambooHR

--- src/resume/resume-page.js
+++ src/resume/resume-page.js
@@ -46,15 +46,15 @@ define({
           <section>
             <h3>Relevant Skills</h3>
             <ul class="list">
+              <li>HTML</li>
+              <li>CSS</li>
               <li>Typescript</li>
               <li>JavaScript</li>
               <li>React.js</li>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>Redux</li>
-              <li>GraphQL</li>
-              <li>Node.js</li>
               <li>Native web components</li>
+              <li>Node.js</li>
+              <li>Webpack/Other build tools</li>
+              <li>CI/CD workflows</li>
             </ul>
           </section>
 
@@ -63,9 +63,9 @@ define({
             <ul class="iconList">
               <li><icon-d20></icon-d20>Dungeons & Dragons</li>
               <li><ion-icon name="cube-outline"></ion-icon>3D modeling/printing</li>
-              <li><ion-icon name="flash-outline"></ion-icon>Electronics</li>
-              <li><ion-icon name="medal-outline"></ion-icon>Gymnastics</li>
               <li><ion-icon name="language-outline"></ion-icon>Japanese</li>
+              <li><ion-icon name="medal-outline"></ion-icon>Gymnastics</li>
+              <li><ion-icon name="flash-outline"></ion-icon>Electronics</li>
             </ul>
           </section>
         </article>
@@ -73,38 +73,45 @@ define({
           <section>
             <h3>Career Objective</h3>
             <p>
-              Enthusiastic and well-rounded front-end engineer with 6+ years of experience building
-              rich applications using Typescript/JavaScript and other web technologies. Eager to
-              support Weave in building features for Weave's desktop clients, writing high-quality
-              code, and pitching in where needed as a member of an autonomous, cross-functional
-              team.
+              Enthusiastic and detail-oriented software engineer with 7+ years of experience
+              building rich applications using Typescript/JavaScript and other web technologies.
+              Eager to support BambooHR in building highly polished and interactive frontend web
+              UIs, coordinating with/mentoring others, and delivering a world-class experience to
+              BambooHR's customers.
             </p>
           </section>
 
           <section>
             <h3>Experience</h3>
-            <h4>Front End Team Lead</h4>
-            <p>DataBased | Lehi, UT | Aug 2018 - Present</p>
+            <h4>Senior Software Engineer</h4>
+            <p>Weave | Lehi, UT | Oct 2020 - Present</p>
             <ul class="list">
               <li>
-                Designed and implemented the UI for DataBased's websites and services in Typescript
-                and React.js
+                Setup a CI pipeline to automatically code sign, notarize, and staple Weave's desktop
+                application
               </li>
-              <li>Wrote and consumed 3 unique company RESTful APIs</li>
-              <li>Setup code bundling, CI/CD pipelines, HTTPS certificates, and hosting</li>
               <li>
-                Developed internal NPM packages for 9 sets of utilities and 12 React components
+                Wrote codemods to dynamically refactor technical debt in 3 separate product
+                repositories
+              </li>
+              <li>
+                Created and maintained components and utilities for an internal design system used
+                in all of Weave's products
               </li>
             </ul>
           </section>
 
           <section>
-            <h4>Software Engineer</h4>
-            <p>Fiddle | Provo, UT | Mar 2017 - Aug 2017</p>
+            <h4>Frontend Team Lead</h4>
+            <p>DataBased | Lehi, UT | Aug 2018 - Present</p>
             <ul class="list">
               <li>
-                Built and published an iOS mobile app using React Native and Redux with a GraphQL
-                backend
+                Designed and implemented the UX/UI for DataBased's websites and services in
+                Typescript and React.js
+              </li>
+              <li>
+                Setup and maintained code bundling, CI/CD pipelines, HTTPS certificates, and server
+                infrastructure
               </li>
             </ul>
           </section>
@@ -114,7 +121,6 @@ define({
             <p>FamilySearch | Orem, UT | Mar 2016 - Aug 2016</p>
             <ul class="list">
               <li>Coordinated with multiple teams to create features used by millions of users</li>
-              <li>Submitted over 130 merge requests for bug fixes and features</li>
             </ul>
           </section>
         </main>
